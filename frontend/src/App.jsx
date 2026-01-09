0f798ee911b376ed152ae60f47f4db8b2dc01323
isaacwatson30 <isaacwatson30@gmail.com>
2026-01-09 10:45:34 -0600

    updated app page for navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,27 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Feed from "./Feed";
-import PostFlowImg from  "./assets/PostFlow.png"
+import Profile from "./Profile";
+
 export default function App() {
   const [user, setUser] = useState(null);
 
   return (
-    <div>
-    <div className= "logo1" >
-      <img src={PostFlowImg} />
-       </div>
-       
-    <div className="app-container">
-      {user === null && <Login onLogin={setUser} />}
-      {user !== null && <Feed user={user} onLogout={() => setUser(null)} />}
-    </div>
-    </div>
+    <Router>
+      <Routes>
+        {!user && <Route path="*" element={<Login onLogin={setUser} />} />}
+
+        {user && (
+          <>
+            <Route
+              path="/"
+              element={<Feed user={user} onLogout={() => setUser(null)} />}
+            />
+            <Route path="/profile" element={<Profile user={user} />} />
+          </>
+        )}
+      </Routes>
+    </Router>
   );
 }
