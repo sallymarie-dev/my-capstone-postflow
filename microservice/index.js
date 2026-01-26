965fac35490a1b0a9c11cde112e2fa5f4d047d2a
Sally <causeysally@gmail.com>
2026-01-26 11:24:10 -0600

    added weather api from visual crossing

--- microservice/index.js
+++ microservice/index.js
@@ -1,15 +1,57 @@
-import express from 'express';
-import 'dotenv/config';
-
+import "dotenv/config";
+import express from "express";
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
-app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ message: "weather microservice is running!!" });
+});
+const myAPIKey = process.env.myAPIKey;
+app.get("/weather", async (req, res) => {
+  const clientKey = req.headers["x-api-key"];
+  const validKey = process.env.SERVICE_KEY;
+  console.log(`Received key: ${clientKey ? "present" : "missing"}`);
+  if (!clientKey || clientKey !== validKey) {
+    return res.status(401).json({ error: "Not authorized" });
+  }
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Microservice is running' });
+  const { zip, date } = req.query;
+  if (!zip) return res.status(400).json({ error: "zip is required" });
+  if (!date) return res.status(400).json({ error: "date is required" });
+  console.log(`Fetching weather for ${zip}, ${date}`);
+  const result = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${zip}/${date}?key=${myAPIKey}&contentType=json`
+  );
+
+  const data = await result.json();
+  console.log(data);
+
+  const days = data.days.reduce((resultDays, day) => {
+    resultDays.push({
+      datetime: day.datetime,
+      temp: day.temp,
+    });
+    return resultDays;
+  }, []);
+
+  // const dayData = data.days[0];
+  // const weatherDTO = {
+  //   date: dayData.datetime,
+  //   tempHigh: dayData.tempmax,
+  //   tempLow: dayData.tempmin,
+  //   conditions: dayData.conditions,
+  // };
+
+  const weather = {
+    tempHigh: 41,
+    tempLow: 28,
+    conditions: "windy, icy",
+    summary: "Cold chill advisory",
+  };
+  res.json(days);
 });
 
 app.listen(PORT, () => {
-  console.log(`Microservice running on port ${PORT}`);
+  console.log(`Weather Microservice running on port ${PORT}`);
 });
+export default app;
